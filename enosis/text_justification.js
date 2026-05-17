7943e266b7b2de77f55fefc789d9e08e82f0800a
Asif Ahmed Sahil <asifahmedsahil.12@gmail.com>
2026-05-17 20:42:23 +0600

    text justification

---
+++ enosis/text_justification.js
@@ -0,0 +1,66 @@
+function textjustification(words, maxWidth) {
+
+ const res = [];
+
+    let line = [];
+
+    let length = 0;
+
+    for (const word of words) {
+
+        // check line complete or not
+        if (length + line.length + word.length > maxWidth) {
+
+            // total extra spaces
+            let extraSpace = maxWidth - length;
+
+            // gaps between words
+            let gaps = Math.max(1, line.length - 1);
+
+            // evenly distributed spaces
+            let spaces = Math.floor(extraSpace / gaps);
+
+            // remaining extra spaces
+            let reminder = extraSpace % gaps;
+
+            // add spaces
+            for (let j = 0; j < gaps; j++) {
+
+                line[j] += " ".repeat(spaces);
+
+                if (reminder > 0) {
+
+                    line[j] += " ";
+
+                    reminder--;
+                }
+            }
+
+            // build justified line
+            res.push(line.join(""));
+
+            // reset
+            line = [];
+
+            length = 0;
+        }
+
+        // add current word
+        line.push(word);
+
+        length += word.length;
+    }
+
+    // handle last line
+    let lastLine = line.join(" ");
+
+    let trailSpaces = maxWidth - lastLine.length;
+
+    res.push(lastLine + " ".repeat(trailSpaces));
+
+    return res;
+}
+
+const output = textjustification(["This", "is", "an", "example", "of", "text", "justification."],16)
+
+console.log(output)
